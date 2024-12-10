72fd5fe5e65e627513741fb94f868d23638bff07
weiseng <weiseng@redairship.com>
2024-12-10 22:54:47 +0800

    feat: d10

---
+++ 2024/10/q2.js
@@ -0,0 +1,40 @@
+const input = (await Deno.readTextFile('input.txt'))
+  .split('\n')
+  .filter(a => a)
+  .map(r => r.split('').map(a => +a))
+// console.log("input:", input);
+
+const walk = (input, number, [i, j]) => {
+  if (input[i] && typeof input[i][j] !== 'undefined' && input[i][j] === number) {
+    if (number === 0) {
+      return [`${i},${j}`]
+    }
+
+    return [
+      ...walk(input, number - 1, [i - 1, j]),
+      ...walk(input, number - 1, [i + 1, j]),
+      ...walk(input, number - 1, [i, j - 1]),
+      ...walk(input, number - 1, [i, j + 1])
+    ].filter((a) => a);
+  }
+
+  return [];
+}
+
+const process = (input) => {
+  let total = 0;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (input[i][j] === 9) {
+        total += walk(input, 9, [i,j]).length;
+      }
+    }
+  }
+  return total;
+}
+
+console.time('d10 q2');
+const res2 = process(input);
+console.log("res2:", res2);
+console.timeEnd('d10 q2');
+
